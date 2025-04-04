b884abc50fba90025f7f992f4953cc2d44225d0a
AdGeeksGe <n.javakhishvili@adgeeks.ge>
2025-04-04 17:49:36 +0400

    design

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ function App() {
 
   return (
     <div className="min-h-screen flex items-center justify-center p-4">
-      <div className="max-w-md w-full bg-white p-8">
+      <div className="max-w-md w-full p-8">
         {/* <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">შეავსე ფორმა</h1> */}
         
         {message && (
